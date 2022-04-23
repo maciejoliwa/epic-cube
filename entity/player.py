92fba1a6f1b1e39cd3867323cb662e8d306b86f4
maciejoliwa <maciejoliwa0906@gmail.com>
2022-04-23 21:49:34 +0200

    Added code in Player class that makes sure player does not go out of bounds

--- entity/player.py
+++ entity/player.py
@@ -17,17 +17,23 @@ class Player(AbstractEntity):
     def update(self, delta: float) -> tp.NoReturn:
         calculated_speed = int(delta * self.speed)
 
+        # Those ifs checking coords are to make sure the player cannot leave outside the window
+
         if is_key_down(KEY_A):
-            self.x -= calculated_speed
+            if self.x > 0:
+                self.x -= calculated_speed
         
         if is_key_down(KEY_D):
-            self.x += calculated_speed
+            if self.x < 992:  # 1024 - 32 (width of window - width of player entity)
+                self.x += calculated_speed
 
         if is_key_down(KEY_S):
-            self.y += calculated_speed
+            if self.y < 544:  # 576 - 32 (height of window - height of player)
+                self.y += calculated_speed
 
         if is_key_down(KEY_W):
-            self.y -= calculated_speed
+            if self.y > 0:
+                self.y -= calculated_speed
 
     def draw(self) -> tp.NoReturn:
         draw_rectangle(self.x, self.y, _ENTITY_SIZE, _ENTITY_SIZE, GREEN)
